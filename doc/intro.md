c7a0c269065f3b4fa5055635d9c34855114da5d0
castevet6 <antti.e.manninen@gmail.com>
2021-12-02 22:51:31 +0200

    First commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to records-collector
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/records_collector/core_test.clj
@@ -0,0 +1,7 @@
+(ns records-collector.core-test
+  (:require [clojure.test :refer :all]
+            [records-collector.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
